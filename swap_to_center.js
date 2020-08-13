213fc6a3ab16e70fc035aa7d348ce7d6f274b3db
Ricky <rickdhanota@gmail.com>
2020-08-12 22:12:07 -0700

    initial commit

---
+++ swap_to_center.js
@@ -0,0 +1,10 @@
+function swap_to_center(arr){
+    for (var i = 0; i < arr.length/2; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length -i -1] = temp;
+    }
+    return arr;
+}
+var y=swap_to_center([1,2,3,4, 5, 6])
+console.log(y);
